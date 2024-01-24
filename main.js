bb236220ed7f21996be2dc2ef01f159c682f96d7
SerhiiIkin <messileonl@gmail.com>
2024-01-24 11:13:51 +0100

    add slider

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import "./js/BurgerMenu.js";
 import "./js/Form.js"
+import Slider from "./js/Slider.js"
+
+
+const config = {
+    selector: ".my-slider",
+    draggable: true,
+    buttons: true,
+    paggination: true,
+    interval: 0,
+    slidesPerView: 1,
+    
+};
+
+Slider (config)
